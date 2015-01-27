9a28466764306b2598af6f9c47b3c3772ab97734
burdara <robbieb@slalom.com>
2015-01-27 14:48:47 -0600

    updates to amazon_gem.sh

--- amazon_gem.sh
+++ amazon_gem.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # This bootstraps Puppet on Amazon Linux (Enterprise Linux 6)
 # It has been tested on Amazon Linux 2014.03 64bit
-
+#
+# Install Puppet using rubygems
+# Amazon Linux has had a bad habit of breaking puppet 3.x due to ruby
+#   upgrades and missing package dependencies
 set -e
 
 if [ "$EUID" -ne "0" ]; then
@@ -9,23 +12,45 @@ if [ "$EUID" -ne "0" ]; then
   exit 1
 fi
 
-if which puppet > /dev/null 2>&1; then
-  echo "Puppet is already installed."
-  exit 0
-fi
+function _info {
+  echo -e "\e[1m\e[34m[INFO ] ${1}\e[0m"
+}
+
+function _error {
+  echo -e "\e[1m\e[31m[ERROR] ${1}\e[0m"	
+}
+
+function _install_package {
+  [[ -z "$1" ]] && _error "missing package_name argument" && return 1
+  yum list installed "$1" &>/dev/null && _info "$1 package is already installed" && return 0
+  _info "installing $1" && yum -y install "$1"
+  local rc=$?
+  [[ $rc -ne 0 ]] && _error "failed to install package $1" && return $?
+  _info "$1 package is installed"
+}
+
+function _install_gem {
+  [[ -z "$1" ]] && _error "missing gem_name argument" && return 1
+  [[ $(gem list "$1") =~ "$1" ]] && _info "$1 gem is already installed" && return 0
+  _info "installing $1" && gem install "$1" --no-rdoc --no-ri
+  local rc=$?
+  [[ $rc -ne 0 ]] && _error "failed to install gem $1" && return $?
+  _info "$1 package is installed"
+}
 
 # Not needed for install but nice to have enabled
 yum-config-manager --enable epel > /dev/null
 
-# Install Puppet using rubygems
-# Amazon Linux has had a bad habit of breaking puppet 3.x due to ruby
-# upgrades and missing package dependencies
-echo "Installing puppet via rubygems"
-yum -y install rubygems ruby-devel augeas augeas-devel augeas-libs > /dev/null || rc=$?
-gem install puppet ruby-augeas ruby-nagios --no-rdoc --no-ri > /dev/null || rc=$?
+_info "Installing puppet via rubygems"
+for _package in rubygems ruby-devel augeas augeas-devel augeas-libs; do 
+  _install_package "$_package" || rc=$?
+done
+for _gem in puppet ruby-augeas ruby-nagios; do
+  _install_gem "$_gem" || rc=$?
+done
 
 if [ -n "$rc" ]; then
-  echo "Puppet failed to install!"
+  _error "Puppet and/or dependencies failed to install!"
   exit $rc
 fi
-echo "Puppet installed!"
+_info "Puppet installed!"
